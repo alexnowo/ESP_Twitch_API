5ca9126cfeeddc9b9f233e1567c0e34eb708361f
Alexandre <alexnowo@gmail.com>
2021-06-09 19:30:39 +0200

    [wip] IsLive function

--- src/TwitchApi.cpp
+++ src/TwitchApi.cpp
@@ -7,7 +7,7 @@ TwitchApi::TwitchApi(WiFiClientSecure &client, char *clientId, char *secret)
 	this->_secret = secret;
 }
 
-bool TwitchApi::getAccessToken()
+bool TwitchApi::GetAccessToken()
 {    
     StaticJsonDocument<RESPONSE_ID_SIZE> response;
 
@@ -17,9 +17,10 @@ bool TwitchApi::getAccessToken()
     params += this->_secret;
     params += "&grant_type=client_credentials";
 
-    #ifdef DEBUG
+    #ifdef DEBUG_TWITCH
         Serial.print(HOST_ID_TWITCH);
         Serial.print(URI_ID_TWITCH);
+        Serial.print("?");
         Serial.println(params);
     #endif
 
@@ -27,7 +28,7 @@ bool TwitchApi::getAccessToken()
     this->_http.addHeader("Content-Type", "application/x-www-form-urlencoded");
     int16_t httpCode = this->_http.POST(params);
     
-    #ifdef DEBUG
+    #ifdef DEBUG_TWITCH
         Serial.print("HTTP Code: ");
         Serial.println(httpCode);
     #endif
@@ -36,7 +37,7 @@ bool TwitchApi::getAccessToken()
     {
         String payload = this->_http.getString();
         
-        #ifdef DEBUG
+        #ifdef DEBUG_TWITCH
             Serial.print("Payload: ");
             Serial.println(payload);
         #endif
@@ -45,17 +46,80 @@ bool TwitchApi::getAccessToken()
         DeserializationError error = deserializeJson(response, payload);
         if(error)
         {
-            #ifdef DEBUG
+            #ifdef DEBUG_TWITCH
                 Serial.print(F("deserializeJson() failed: "));
                 Serial.println(error.f_str());
             #endif
             return false;
         }
         strcpy(this->_accessToken, response["access_token"]);
-        #ifdef DEBUG
+        #ifdef DEBUG_TWITCH
             Serial.print("Access token: ");
             Serial.println(_accessToken);
         #endif
+    }
+    else
+    {
+        this->_http.end();
+        return false;
+    }
+    return true;
+}
+
+bool TwitchApi::IsLive(char *streamerName)
+{
+    #ifdef DEBUG_TWITCH
+        Serial.print("Streamer name: ");
+        Serial.println(streamerName);
+    #endif
+
+    String headerAcessToken = "Bearer ";
+    headerAcessToken += this->_accessToken;
+
+    
+    String path = URI_SEARCH_TWITCH;
+    path += "?user_login=";
+    path += streamerName;
+
+    #ifdef DEBUG_TWITCH
+        Serial.println(path);
+        Serial.println("Header:");
+        Serial.println(headerAcessToken);
+    #endif
+
+    this->_http.begin(this->_client, HOST_API_TWITCH, PORT, path);
+    this->_http.addHeader("Authorization", headerAcessToken);
+    this->_http.addHeader("Client-Id", this->_clientId);
+    
+    int16_t httpCode = this->_http.GET();
+    #ifdef DEBUG_TWITCH
+        Serial.print("HTTP Code: ");
+        Serial.println(httpCode);
+    #endif
+    if(httpCode == HTTP_CODE_OK)
+    {
+        String payload = this->_http.getString();
+        
+        #ifdef DEBUG_TWITCH
+            Serial.print("Payload: ");
+            Serial.println(payload);
+        #endif
+        this->_http.end();
+
+/*         DeserializationError error = deserializeJson(response, payload);
+        if(error)
+        {
+            #ifdef DEBUG_TWITCH
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.f_str());
+            #endif
+            return false;
+        }
+        strcpy(this->_accessToken, response["access_token"]);
+        #ifdef DEBUG_TWITCH
+            Serial.print("Access token: ");
+            Serial.println(_accessToken);
+        #endif */
     }
     else
     {
--- src/TwitchApi.h
+++ src/TwitchApi.h
@@ -6,62 +6,25 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-#define DEBUG
+#define DEBUG_TWITCH
 
-#define HOST_API_TWITCH "api.twitch.tv"
-#define HOST_ID_TWITCH  "id.twitch.tv"
-#define URI_ID_TWITCH   "/oauth2/token"
+#define HOST_API_TWITCH   "api.twitch.tv"
+#define URI_SEARCH_TWITCH "/helix/search/streams"
+
+#define HOST_ID_TWITCH    "id.twitch.tv"
+#define URI_ID_TWITCH     "/oauth2/token"
 #define PORT 443
 
 #define RESPONSE_ID_SIZE 192
 #define ACCESS_TOKEN_SIZE 30
 
-/* struct UserData
-{
-    char *id;
-    char *login;
-    char *displayName;
-    char *type;
-    char *broadCasterType;
-    char *description;
-    char *profileImageUrl;
-    char *offlineImageUrl;
-    long viewCount;
-    bool error;
-};
-
-struct FollowerData
-{
-    long total;
-    char *fromId;
-    char *fromName;
-    char *toId;
-    char *toName;
-    char *followedAt;
-    bool error;
-};
-
-struct StreamInfo
-{
-    char *id;
-    char *userId;
-    char *userName;
-    char *gameId;
-    char *type;
-    char *title;
-    long viewerCount;
-    char *startedAt;
-    char *language;
-    char *thumbnailUrl;
-    bool error;
-}; */
-
 class TwitchApi
 {
   public:
     TwitchApi(WiFiClientSecure &client, char *clientId, char *secret);
-    bool getAccessToken();
-    bool debug = false;
+    bool GetAccessToken();
+    bool IsLive(char *streamerName);
+    bool status = false;
 
   private:
     WiFiClientSecure _client;
